868c72929c222814c32806f2cd8815b6c5efc0ce
Akram <m.akram.arar@gmail.com>
2020-03-11 16:28:18 +0100

    debut personnal

---
+++ src/main/java/PGL/pglp_4_1/Personnal.java
@@ -0,0 +1,25 @@
+package PGL.pglp_4_1;
+import java.time.LocalDate;
+
+public class Personnal {
+	
+	private final String nom = "";
+	private final String prenom = "";
+	private final String fonction = "";
+	private final LocalDate dateNaissance = null;
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public String getFonction() {
+		return fonction;
+	}
+	public LocalDate getDateNaissance() {
+		return dateNaissance;
+	}
+
+}
