291a66d112b252c7a3f810aa4564934003d10967
Akram <m.akram.arar@gmail.com>
2020-03-28 02:12:10 +0100

    Iterator

--- src/main/java/PGL/pglp_4_1/GroupePersonnel.java
+++ src/main/java/PGL/pglp_4_1/GroupePersonnel.java
@@ -1,18 +1,26 @@
 package PGL.pglp_4_1;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class GroupePersonnel implements Component{
+	
     private final ArrayList<Component> childComp = new ArrayList<Component>();
-
     public void add(Component comp) {
     	childComp.add(comp);
     }
-    
+    public void remove(final Component composant) {
+    	childComp.remove(composant);
+    }
     public void print() {
-        for (Component graphic : childComp) {
-            graphic.print(); 
+        for (Component cm : childComp) {
+            cm.print(); 
         }
     }
-
+    
+    // Iterator
+	public Iterator<Component> iterator() {
+		
+		return childComp.iterator();
+	}
 }
